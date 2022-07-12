cdc64f3fba134dce66b28ab4bf4b3d2ceb86b75d
Lukasz Adamczyk <lu.adamczyk97@gmail.com>
2022-07-12 22:23:58 +0200

    Update BarkingDog.java

--- src/BarkingDog.java
+++ src/BarkingDog.java
@@ -1,8 +1,5 @@
 public class BarkingDog {
     public static boolean shouldWakeUp(boolean barking, int hourOfDay) {
-        if (barking && ((hourOfDay < 8 || hourOfDay > 22))
-            return true;
-        else
-            return false;
+        return (barking && ((hourOfDay < 8 || hourOfDay > 22))
     }
 }
